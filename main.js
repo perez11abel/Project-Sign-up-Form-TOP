54de9ef47809210c10543219ce391ddcfc475839
abel <abel_perez9@hotmail.com>
2022-12-16 11:21:03 -0600

    finished image part

---
+++ main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let password = document.querySelector('#password').value;
+    let confirmPassword = document.querySelector('#confirmPassword').value;
+
+    let error = document.querySelector('#error');
+
+    if (password == "" && confirmPassword == "") {
+        error.innerHTML = '*passwords do not match';
+    } else if(password === confirmPassword) {
+        error.innerHTML = '';
+    }
+})
